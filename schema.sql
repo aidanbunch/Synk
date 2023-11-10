86dfb7eeda432029aeb561fcaef64de9ea84f6c0
aidanbunch <aidanb08@icloud.com>
2023-11-10 15:35:21 -0800

    created rough db schema, junction tables for many-to-many relationships

--- schema.sql
+++ schema.sql
@@ -43,3 +43,84 @@ $$ language plpgsql security definer;
 create trigger on_auth_user_updated
   after update of "email" on auth.users
   for each row execute procedure public.handle_update_user();
+
+/*** EVENT FLOWS ***/
+
+create table public.event_flows (
+  "id" uuid primary key default uuid_generate_v4(),
+  "owner" uuid references public.users not null,
+  "event_name" text,
+  "num_attendees" integer,
+  "budget" numeric,
+  "description" text,
+  "hq_location" text,
+  "start_date" date,
+  "end_date" date,
+  constraint "event_name" check (char_length("event_name") >= 1 OR char_length("event_name") <= 144)
+);
+
+alter table public.event_flows enable row level security;
+create policy "Can view their event flows" on public.event_flows for select using ( auth.uid() = "owner" );
+create policy "Can insert their event flows" on public.event_flows for insert with check ( auth.uid() = "owner" );
+create policy "Can update their event flows" on public.event_flows for update using ( auth.uid() = "owner" );
+create policy "Can delete their event flows" on public.event_flows for delete using ( auth.uid() = "owner" );
+
+/*** HOTELS ***/
+
+create table public.hotels (
+  "id" uuid primary key default uuid_generate_v4(),
+  "hotel_info" jsonb
+);
+
+/*** FLIGHTS ***/
+
+create table public.flights (
+  "id" uuid primary key default uuid_generate_v4(),
+  "flight_info" jsonb
+);
+
+/*** ACTIVITIES ***/
+
+create table public.activities (
+  "id" uuid primary key default uuid_generate_v4(),
+  "activity_info" jsonb
+);
+
+/*** EVENT_FLOW_HOTELS ***/
+
+create table public.event_flow_hotels (
+  "event_flow" uuid references public.event_flows not null,
+  "hotel" uuid references public.hotels not null,
+  primary key ("event_flow", "hotel")
+);
+
+alter table public.event_flow_hotels enable row level security;
+create policy "Can view their event_flow_hotels" on public.event_flow_hotels for select using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can insert their event_flow_hotels" on public.event_flow_hotels for insert with check ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can delete their event_flow_hotels" on public.event_flow_hotels for delete using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+
+/*** EVENT_FLOW_FLIGHTS ***/
+
+create table public.event_flow_flights (
+  "event_flow" uuid references public.event_flows not null,
+  "flight" uuid references public.flights not null,
+  primary key ("event_flow", "flight")
+);
+
+alter table public.event_flow_flights enable row level security;
+create policy "Can view their event_flow_flights" on public.event_flow_flights for select using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can insert their event_flow_flights" on public.event_flow_flights for insert with check ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can delete their event_flow_flights" on public.event_flow_flights for delete using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+
+/*** EVENT_FLOW_ACTIVITIES ***/
+
+create table public.event_flow_activities (
+  "event_flow" uuid references public.event_flows not null,
+  "activity" uuid references public.activities not null,
+  primary key ("event_flow", "activity")
+);
+
+alter table public.event_flow_activities enable row level security;
+create policy "Can view their event_flow_activities" on public.event_flow_activities for select using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can insert their event_flow_activities" on public.event_flow_activities for insert with check ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
+create policy "Can delete their event_flow_activities" on public.event_flow_activities for delete using ( auth.uid() = (select "owner" from public.event_flows where "id" = "event_flow") );
