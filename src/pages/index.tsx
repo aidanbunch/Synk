d9978aa439a2da7529d15c21a58106de5c3417b9
aidanbunch <aidanb08@icloud.com>
2023-11-29 15:13:14 -0800

    added demo vid to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,26 @@ export default function Home() {
 				<Container py={{ base: "16", md: "24" }}>
 					<Stack spacing={{ base: "8", md: "10" }}>
 						<Stack spacing={{ base: "4", md: "5" }} align="center">
-							<Heading size="xl">Event Planning, Made Easy</Heading>
-							<Text color="fg.muted" maxW="xl" textAlign="center" fontSize="lg">
+							<Heading size="3xl" textAlign="center" minW="1000" pt="15" pb="6">Event Planning, Made Easy</Heading>
+							<video
+								src="https://qwusjyzfofxtckzzaexg.supabase.co/storage/v1/object/public/VideoBucket/synk-demo.mp4"
+								loop
+								autoPlay
+								muted
+								playsInline
+								style={{
+									width: "100%",
+									marginTop: "10px",
+									height: "100%",
+									borderRadius: "8px",
+									display: "block",
+									objectFit: "cover",
+									backgroundColor: "rgba(0,0,0,0)",
+									objectPosition: "50% 50%",
+									boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+								}}
+							></video>
+							<Text color="fg.muted" maxW="xl" textAlign="center" fontSize="lg" pt="5">
 								Plan your next event with ease using our automated platform.
 								From scheduling to execution, we&apos;ve got you covered.
 							</Text>
